1e386cbebb6d8cd15b5bd96b8df3753c106aecc9
Nanaya Miki <nanayamiki@gmail.com>
2023-04-07 14:21:37 +0000

    completed the challenge

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+	//recursive way
+	if (fromN === toN) {
+		return fromN;
+	} else {
+		return fromN + sum(fromN + 1, toN);
+	}
+	/*
+  //regular way
+	let result = 0;
+	for (let i = fromN; i <= toN; i++) {
+		result += i;
+	}
+	return result;
+  */
 }
 
 module.exports = sum;
+
+//gpt
+//https://chat.openai.com/chat/dc3abb24-cd13-4c75-9ca2-51290e438150
